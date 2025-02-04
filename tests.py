517da0553070559b7796472cd9f8a1d0bdf84264
Sergey Zhukov <szhukov@aligntech.com>
2025-02-04 17:22:31 +0300

    refactor: use module in __init__

--- tests.py
+++ tests.py
@@ -1,18 +1,19 @@
+from source import select_all_exclude, get_columns_names
+
 import datafusion
-import source.utils as utils
 
 
 def test_get_columns_names():
     ctx = datafusion.SessionContext()
     df = ctx.from_pydict({"id": [1, 2, 3], "name": ["foo", "bar", "baz"], "data": [42, 43, 44]})
-    columns_names = utils.get_columns_names(df)
+    columns_names = get_columns_names(df)
     assert columns_names == ["id", "name", "data"]
 
 
 def test_select_all_exclude():
     ctx = datafusion.SessionContext()
     df = ctx.from_pydict({"id": [1, 2, 3], "name": ["foo", "bar", "baz"], "data": [42, 43, 44]})
-    res = utils.select_all_exclude(df, ["name", "data"])
+    res = select_all_exclude(df, ["name", "data"])
     res = res.to_pydict()
     assert res == {"id": [1, 2, 3]}
 
@@ -20,3 +21,4 @@ def test_select_all_exclude():
 if __name__ == "__main__":
     test_get_columns_names()
     test_select_all_exclude()
+    
