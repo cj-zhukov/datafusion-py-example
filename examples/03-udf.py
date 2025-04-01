f483bb4f5b625c04a7854aaa1dec5360d2b32d39
Sergey Zhukov <szhukov@aligntech.com>
2025-04-01 14:58:06 +0300

    fix: more efficient operation

--- examples/03-udf.py
+++ examples/03-udf.py
@@ -1,11 +1,11 @@
 from datafusion import udf, SessionContext, functions as f, col
+import pyarrow.compute as pc
 import pyarrow
 
 
 def split_text(array: pyarrow.Array) -> pyarrow.Array:
-    xs = array.to_pylist()
-    res = [x.split(".")[0] for x in xs]
-    return pyarrow.array(pyarrow.array(res))
+    split_result = pc.split_pattern(array, ".")  
+    return pc.list_element(split_result, 0)  
 
 
 if __name__ == "__main__":
@@ -13,10 +13,10 @@ if __name__ == "__main__":
     df = ctx.from_pydict({"id": [1, 2, 3], "name": ["foo.txt", "bar.txt", "baz.txt"]})
     exec = udf(split_text, [pyarrow.string()], pyarrow.string(), "stable")
     res = df.select(col("id"), col("name"), exec(f.col("name")).alias("name_new"))
-    print(res)
+    res.show()
 
     ctx.from_pydict({"id": [1, 2, 3], "name": ["foo.txt", "bar.txt", "baz.txt"]}, name="t")
     exec = udf(split_text, [pyarrow.string()], pyarrow.string(), "stable", name="executor")
     ctx.register_udf(exec)
     res = ctx.sql("select id, name, executor(name) as name_new from t")
-    print(res)
+    res.show()
